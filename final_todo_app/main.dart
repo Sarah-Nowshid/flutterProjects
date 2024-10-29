56be9cb661ae98e3ae8b1350e126a5d11a541751
Sarah-Nowshid <157949591+Sarah-Nowshid@users.noreply.github.com>
2024-10-30 00:28:51 +0500

    Create main.dart

---
+++ final_todo_app/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'screens/login_screen.dart';
+import 'firebase_options.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  
+  FirebaseFirestore.instance.settings = const Settings(persistenceEnabled: true); // Enable offline persistence
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'To-Do App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginScreen(),
+    );
+  }
+}
